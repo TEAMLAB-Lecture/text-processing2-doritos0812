9e27a0d8bd08fc0e2470b6cf999cd98377b775aa
h_mini <doritos2498@gmail.com>
2021-01-19 16:39:25 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -72,8 +72,8 @@ def to_camel_case(underscore_str):
             "alreadyCamel"
     """
     origin = underscore_str[:]
-    if len(underscore_str.split('_')) == 1:
-        return origin
+    if len(origin.split('_')) == 1:
+        return underscore_str
     result = ''.join([x.lower().title() for x in underscore_str.split('_')])
     return result[0].lower() + result[1:]
 
